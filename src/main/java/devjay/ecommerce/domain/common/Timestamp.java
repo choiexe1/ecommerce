ca8e587d8322abab393894fe103616c7be2639fe
choiexe1 <choiexe1@gmail.com>
2024-12-10 20:05:31 +0900

    Timestamp 속성에 @DateTimeFormat 적용

--- src/main/java/devjay/ecommerce/domain/common/Timestamp.java
+++ src/main/java/devjay/ecommerce/domain/common/Timestamp.java
@@ -3,11 +3,18 @@ package devjay.ecommerce.domain.common;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
 public class Timestamp {
+
+    @DateTimeFormat
     private final LocalDateTime createdAt = LocalDateTime.now();
+
+    @DateTimeFormat
     private LocalDateTime updatedAt;
+
+    @DateTimeFormat
     private LocalDateTime deletedAt;
 }
