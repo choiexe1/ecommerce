4ac8943ce955ec84c067eb6e614fd8d0e2148571
choiexe1 <choiexe1@gmail.com>
2024-12-10 18:43:09 +0900

    AddressTest 메서드 이름 수정
    한글 -> 영어로

--- src/test/java/devjay/ecommerce/domain/address/AddressTest.java
+++ src/test/java/devjay/ecommerce/domain/address/AddressTest.java
@@ -8,7 +8,7 @@ class AddressTest {
 
     @Test
     @DisplayName("Province가 지원하지 않는 City로 Address 생성 시 예외가 발생해야 한다.")
-    void 유효하지_않은_주소_예외발생() {
+    void invalidProvinceCreation() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             new Address(Province.SEOUL, City.PAJU, "오류동", "테스트로", "1번지");
         });
@@ -16,14 +16,14 @@ class AddressTest {
 
     @Test
     @DisplayName("유효한 Province와 City 조합으로 Address가 정상 생성되어야 한다.")
-    void 올바른_주소_생성() {
+    void validProvinceCreation() {
         Address address = new Address(Province.SEOUL, City.GANGNAM, "삼성동", "테스트로", "1번지");
         Assertions.assertNotNull(address);
     }
 
     @Test
     @DisplayName("모든 값이 같으면 동일한 주소로 판단한다.")
-    void 동일한_주소_비교() {
+    void equals() {
         // GIVEN
         Address addressA = new Address(Province.GYEONGGI, City.PAJU, "금촌동", "테스트로", "1번지");
         Address addressB = new Address(Province.GYEONGGI, City.PAJU, "금촌동", "테스트로", "1번지");
@@ -37,7 +37,7 @@ class AddressTest {
 
     @Test
     @DisplayName("속성이 하나라도 다르면 다른 주소로 판단한다.")
-    void 다른_주소_비교() {
+    void notEquals() {
         // GIVEN
         Address addressA = new Address(Province.GYEONGGI, City.PAJU, "금촌동", "테스트로", "1번지");
         Address addressB = new Address(Province.GYEONGGI, City.PAJU, "금촌동", "테스트로", "2번지");
