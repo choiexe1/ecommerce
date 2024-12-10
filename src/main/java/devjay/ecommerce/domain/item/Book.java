0805a78658ba99dd8b480148e22c731d78a6e49e
choiexe1 <choiexe1@gmail.com>
2024-12-10 19:57:50 +0900

    Book 코드 수정

--- src/main/java/devjay/ecommerce/domain/item/Book.java
+++ src/main/java/devjay/ecommerce/domain/item/Book.java
@@ -1,18 +1,23 @@
 package devjay.ecommerce.domain.item;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Book extends Item {
-    @NotNull
     private String publisher;
     private String isbn;
     private String author;
 
     public Book(String name, int price, int quantity) {
         super(name, price, quantity);
+    }
+
+    public Book(String name, int price, int quantity, String publisher, String isbn, String author) {
+        super(name, price, quantity);
+        this.publisher = publisher;
+        this.isbn = isbn;
+        this.author = author;
     }
 }
